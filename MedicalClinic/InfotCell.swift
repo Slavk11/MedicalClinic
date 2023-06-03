e3e666bbbe3f0a8684420022895472f551873ca1
Stanislav Sazonov <slavk11@yandex.ru>
2023-06-03 16:46:45 +0300

    Created prototype for app

---
+++ MedicalClinic/InfotCell.swift
@@ -0,0 +1,15 @@
+//
+//  ContentCell.swift
+//  MedicalClinic
+//
+//  Created by Сазонов Станислав on 03.06.2023.
+//
+
+import UIKit
+
+final class InfoCell: UITableViewCell {
+    @IBOutlet var newsTitleLabel: UILabel!
+    @IBOutlet var newsImageLabel: UIImageView!
+    @IBOutlet var newsTextLabel: UILabel!
+    
+}
--- MedicalClinic/ViewController.swift
+++ MedicalClinic/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class HomePageViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "infoCell", for: indexPath)
+        guard let cell = cell as? InfoCell else { return UITableViewCell() }
+        var content = cell.defaultContentConfiguration()
+        return cell
+
+    }
 
 }
 
