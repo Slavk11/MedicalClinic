4f98ce31639212a08714395e72c5adfa2e2b6506
Stanislav Sazonov <slavk11@yandex.ru>
2023-06-04 16:55:18 +0300

    added models data

--- MedicalClinic/MainViewController.swift
+++ MedicalClinic/MainViewController.swift
@@ -7,23 +7,24 @@
 
 import UIKit
 
-final class HomePageViewController: UITableViewController {
+final class MainViewController: UITableViewController {
+    let model = Hospital.getInfo()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        model.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "infoCell", for: indexPath)
         guard let cell = cell as? InfoCell else { return UITableViewCell() }
-        var content = cell.defaultContentConfiguration()
+        let model = model[indexPath.row]
+        cell.configure(with: model)
         return cell
-
+        
     }
 
 }
