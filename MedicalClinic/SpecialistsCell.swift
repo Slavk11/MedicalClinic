93a2333c2e8511972ccdaf1cf1729fa71dc4a600
Stanislav Sazonov <slavk11@yandex.ru>
2023-06-04 16:57:15 +0300

    added custom class for table view

---
+++ MedicalClinic/SpecialistsCell.swift
@@ -0,0 +1,17 @@
+//
+//  SpecialistsCell.swift
+//  MedicalClinic
+//
+//  Created by Сазонов Станислав on 04.06.2023.
+//
+
+import UIKit
+
+class SpecialistsCell: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
