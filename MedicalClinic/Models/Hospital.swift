3870ce47dfc292402bbd64ede21cac6255c6f3f2
Stanislav Sazonov <slavk11@yandex.ru>
2023-06-11 19:48:22 +0300

    Added new analyzes data, also changed counter for 5 and 10 index

--- MedicalClinic/Models/Hospital.swift
+++ MedicalClinic/Models/Hospital.swift
@@ -9,25 +9,25 @@ struct Hospital {
     let newsTitle: String
     let newsText: String
     let newsImage: String
+    let analyzesTitle: String
     
     static func getInfo() -> [Hospital] {
-            var news: [Hospital] = []
+        var news: [Hospital] = []
         
-            let newsTitle = DataStore.shared.newsTitle
-            let newsText = DataStore.shared.newsText
-        let newsImage = DataStore.shared.newsImage
+        let newsTitle = DataStore.shared.newsTitles
+        let newsText = DataStore.shared.newsTexts
+        let newsImage = DataStore.shared.newsImages
+        let analyzesTitle = DataStore.shared.analyzesTitles
         
-            
-            for index in 0..<newsTitle.count {
-                let model = Hospital(
-                    
-                    newsTitle: newsTitle[index],
-                    newsText: newsText[index],
-                    newsImage: newsImage[index]
-                )
-                news.append(model)
-            }
-            
-            return news
+        for index in 0..<analyzesTitle.count {
+            let model = Hospital(
+                newsTitle: newsTitle[index % newsTitle.count],
+                newsText: newsText[index % newsText.count],
+                newsImage: newsImage[index % newsImage.count],
+                analyzesTitle: analyzesTitle[index]
+            )
+            news.append(model)
         }
+        return news
+    }
 }
