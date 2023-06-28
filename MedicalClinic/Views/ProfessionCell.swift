9583b328d1ea36200882f17af3d39be30108006f
Stanislav Sazonov <slavk11@yandex.ru>
2023-06-28 22:25:42 +0300

    Added new cell for Profession View

---
+++ MedicalClinic/Views/ProfessionCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProfessionCell.swift
+//  MedicalClinic
+//
+//  Created by Сазонов Станислав on 28.06.2023.
+//
+
+import UIKit
+
+final class ProfessionCell: UITableViewCell {
+    @IBOutlet var professionLabel: UILabel!
+    @IBOutlet var professionImage: UIImageView!
+    
+    func configure(with model: Hospital) {
+        professionLabel.text = model.professions
+    }
+}
