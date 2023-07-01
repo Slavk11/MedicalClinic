03bb87763d6160181a5868b5ea657ef70d8b8918
Stanislav Sazonov <slavk11@yandex.ru>
2023-07-01 22:49:31 +0300

    Created Authorization View and set elements to Authorization view

---
+++ MedicalClinic/ViewControllers/AuthorizationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AuthorizationViewController.swift
+//  MedicalClinic
+//
+//  Created by Сазонов Станислав on 01.07.2023.
+//
+
+import UIKit
+
+final class AuthorizationViewController: UIViewController {
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
