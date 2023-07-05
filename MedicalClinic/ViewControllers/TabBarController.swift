63b11dfce08bf7d146fa2142b08247e18cdbab9b
Stanislav Sazonov <slavk11@yandex.ru>
2023-07-05 20:49:51 +0300

    Added class for TabBarController, created transferData func for send data to Views

---
+++ MedicalClinic/ViewControllers/TabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarController.swift
+//  MedicalClinic
+//
+//  Created by Сазонов Станислав on 05.07.2023.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    private let hospital = Hospital.getInfo()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        transferData()
+    }
+    
+    private func transferData() {
+        guard let viewControllers else { return }
+        
+        viewControllers.forEach {
+            if let newsVc = $0 as? NewsViewController {
+                newsVc.hospital = hospital
+            } else if let analyzesGroupVC = $0 as? AnalyzesGroupViewController {
+                analyzesGroupVC.hospital = hospital
+            } else if let professionsVC = $0 as? ProfessionsViewController {
+                professionsVC.hospital = hospital
+            } else if let authVC = $0 as? AuthorizationViewController {
+                authVC.hospital = hospital
+            }
+        }
+    }
+    
+}
