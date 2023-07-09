6cc5d8e40bda0d39dd7d3ebea36c5ac736c29e46
Stanislav Sazonov <slavk11@yandex.ru>
2023-07-09 14:50:53 +0300

    Created new class for patients accounts, added 2 required func for table view cell

---
+++ MedicalClinic/ViewControllers/PatientAppointmentsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PatientAppointmentViewController.swift
+//  MedicalClinic
+//
+//  Created by Сазонов Станислав on 09.07.2023.
+//
+
+import UIKit
+
+final class PatientAppointmentsViewController: UITableViewController {
+    
+    var hospital: [Hospital] = []
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "appointmentsCell", for: indexPath)
+        guard let cell = cell as? PatientAppointmentsCell else { return UITableViewCell ()}
+        let model = hospital[indexPath.row]
+        cell.configure(with: model)
+        return cell
+    }
+    
+}
